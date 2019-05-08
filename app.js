618c78639ad464065c18dfb5fc4889a5acca665e
CSH0823 <2169265216@qq.com>
2019-05-08 16:48:08 +0800

    first

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 const fs = require('fs');
 const http = require('http');
-const mime = require('mime');
-const atrtemplate = require('art-template');
+// const mime = require('mime');
+// const atrtemplate = require('art-template');
 
 http
 .createServer((req,res)=>{
-    res.end('ok')
+    const url = req.url
+    if(url==='/'){
+        fs.readFile('./index.html',(err,data)=>{
+            if(err){
+              return  res.end("404")
+            }
+            res.setHeader('content-type','text/html;charset=utf-8')
+            return res.end(data.toString())
+        })
+
+
+    }else if(url==='/post.html'){
+        fs.readFile('./post.html',(err,data)=>{
+            if(err){
+              return  res.end("404")
+            }
+            res.setHeader('content-type','text/html;charset=utf-8')
+            return res.end(data.toString())
+        })
+
+    }else if(url.startsWith('/public/')){
+
+        const path = `.${url}`
+        fs.readFile(path,(err,data)=>{
+            if(err){
+                return res.end('404')
+            }
+            res.setHeader('Content-type','text/css;charset=utf-8')
+            res.end(data.toString())
+        })
+    }
+
+
+
+
+
+
+
 })
 .listen(80,()=>{
     console.log('running...')
