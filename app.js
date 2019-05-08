6dfb458cd56c9723a9065c234b01ba7afe5597ad
CSH0823 <2169265216@qq.com>
2019-05-08 15:21:54 +0800

    test

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const http = require('http');
 const mime = require('mime');
 const atrtemplate = require('art-template');
 
+http
+.createServer((req,res)=>{
+    res.end('ok')
+})
+.listen(80,()=>{
+    console.log('running...')
+})
