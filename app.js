b30ba28274ba302c3383f528271ae0163087be22
CSH0823 <2169265216@qq.com>
2019-05-09 17:47:49 +0800

    artTemplate

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 const fs = require('fs');
 const http = require('http');
-// const mime = require('mime');
-// const atrtemplate = require('art-template');
+const mime = require('mime');
+const atrTemplate = require('art-template');
+// const dbPath = './db.json';
+
+const comments=[
+    {
+        name:"曹少华",
+        content:"三十年河东三十年河西，莫欺少年穷"
+    },
+    {
+        name:"江左梅郎",
+        // email:"2169265216@qq.com",
+        // tel:18910525823,
+        content:"三十年河东三十年河西，莫欺少年穷"
+    },
+    {
+        name:"佑卿",
+        // email:"2169265216@qq.com",
+        // tel:18910525823,
+        content:"三十年河东三十年河西，莫欺少年穷"
+    },
+    {
+        name:"从别后",
+        // email:"2169265216@qq.com",
+        // tel:18910525823,
+        content:"三十年河东三十年河西，莫欺少年穷"
+    }
+]
+
 
 http
 .createServer((req,res)=>{
     const url = req.url
-    if(url==='/'){
+    const method = req.method.toLowerCase();
+    if(method==='get' && url==='/'){
         fs.readFile('./index.html',(err,data)=>{
             if(err){
               return  res.end("404")
             }
+            data = atrTemplate.render(data.toString(),{comments:comments})
             res.setHeader('content-type','text/html;charset=utf-8')
-            return res.end(data.toString())
+            return res.end(data)
+        
         })
 
-
-    }else if(url==='/post.html'){
+    }else if(method==='get' && url==='/post'){
         fs.readFile('./post.html',(err,data)=>{
             if(err){
               return  res.end("404")
@@ -25,15 +54,35 @@ http
             return res.end(data.toString())
         })
 
-    }else if(url.startsWith('/public/')){
+    }else if(method==='get' && (url.startsWith('/public/')||url.startsWith('/node_modules/'))){
 
         const path = `.${url}`
         fs.readFile(path,(err,data)=>{
             if(err){
                 return res.end('404')
             }
-            res.setHeader('Content-type','text/css;charset=utf-8')
+            res.setHeader('Content-type',mime.getType(path))
             res.end(data.toString())
+        })
+    }else if(method==='post' && url==='/post'){
+
+        const shuRuBodyDataStr = ''
+        req.on('data',chunk=>{
+            shuRuBodyDataStr+=chunk
+        })
+
+        req.on('end',()=>{
+            const shuRu = queryString.parse(shuRuBodyDataStr) 
+
+
+
+
+
+
+            fs.readFile('./index.html',(err,data)=>{
+
+            })
+
         })
     }
 
